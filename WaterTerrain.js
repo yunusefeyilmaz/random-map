a14eb9485e1a2900f4bca9386c53e8e952aa5f8e
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-03-20 20:41:49 +0300

    Refactor createWater function and update water height in index.html

--- WaterTerrain.js
+++ WaterTerrain.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-export const createWater =(widthMap,heightMap)=>{
+export const createWater =(widthMap,heightMap,waterHeight)=>{
     const waterGeometry = new THREE.PlaneGeometry(widthMap, heightMap, 1, 1);
     const waterMaterial = new THREE.MeshStandardMaterial({
       color: 0x0000ff,
@@ -10,7 +10,7 @@ export const createWater =(widthMap,heightMap)=>{
   
     const water = new THREE.Mesh(waterGeometry, waterMaterial);
     water.rotation.x = -Math.PI / 2; // Rotate for a flat terrain
-    water.position.y = -55; // Adjust height
+    water.position.y = waterHeight; // Adjust height
   
     return water;
 }
--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Chart from 'chart.js/auto';
-// Terrain import edilmiyor, doğrudan THREE.Terrain kullanılıyor
+import Chart from "chart.js/auto";
+import { SimplexNoise } from "three/examples/jsm/math/SimplexNoise.js";
 
 // Setup
-const widthMap = 500;
-const heightMap = 500;
+const widthMap = 100;
+const heightMap = 100;
 let mappedValues = [];
+let terrainMesh;
+let start1;
+let stop1;
+let waterHeight=-49.5;
+let stop2;
+let soft;
+let noiseScale;
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize((window.innerWidth *75)/100, window.innerHeight*80/100);
+renderer.setSize(
+  (window.innerWidth * 75) / 100,
+  (window.innerHeight * 80) / 100
+);
 camera.position.setZ(30);
 camera.position.setX(-3);
-
 renderer.render(scene, camera);
+
 
 // Lights
 const pointLight = new THREE.PointLight(0xffffff);
@@ -42,26 +50,36 @@ scene.background = spaceTexture;
 
 // Terrain
 // Import SimplexNoise from Three.js
-import { SimplexNoise } from "three/examples/jsm/math/SimplexNoise.js";
 
-let terrainMesh;
-let start1;
-let stop1;
-let start2;
-let stop2;
-let soft;
-let noiseScale;
 function controlValues() {
   start1 = document.getElementById("start1").value;
   stop1 = document.getElementById("stop1").value;
-  start2 = document.getElementById("start2").value;
+  waterHeight = document.getElementById("start2").value;
   stop2 = document.getElementById("stop2").value;
   soft = document.getElementById("soft").value;
   noiseScale = document.getElementById("noiseScale").value;
 }
-import {createWater} from './WaterTerrain.js';
-scene.add(createWater(widthMap,heightMap));
 
+
+
+// Water Terrain
+import { createWater } from "./WaterTerrain.js";
+let mesh;
+function water(){ 
+  if(mesh){
+    scene.remove(mesh);
+  }
+  const wmesh=createWater(widthMap, heightMap,waterHeight);
+  scene.add(wmesh);
+  mesh=wmesh;
+};
+water(mesh);
+const waterRefresh = 100;
+setInterval(water, waterRefresh);
+
+
+// Create a SimplexNoise object
+const Noise = new SimplexNoise();
 function createTerrain() {
   if (terrainMesh) {
     scene.remove(terrainMesh);
@@ -73,43 +91,52 @@ function createTerrain() {
     stop1
   );
   const terrainMaterial = new THREE.MeshStandardMaterial({
-    vertexColors: true, 
+    vertexColors: true,
     color: 0x554124,
     wireframe: false,
     side: THREE.DoubleSide,
     transparent: true,
     opacity: 1,
   });
-  // Create a SimplexNoise object
-  const simplexNoise = new SimplexNoise();
-  const colors = new Float32Array(terrainGeometry.attributes.position.array.length * 3); // Array for vertex colors
+
+  const colors = new Uint8Array(
+    terrainGeometry.attributes.position.array.length * 3
+  );
 
   for (
     let i = 0;
     i < terrainGeometry.attributes.position.array.length;
     i += 3
   ) {
+
     const x = terrainGeometry.attributes.position.array[i];
     const y = terrainGeometry.attributes.position.array[i + 1];
+
+    const nx = x/widthMap - 0.5; 
+    const ny = y/heightMap - 0.5;
     let noise = 0;
+
     // Calculate the noise value
-    noise = simplexNoise.noise(x / soft, y / soft) * noiseScale;
+    const e =
+      1 * Noise.noise(1 * nx, 1 * ny) +
+      0.5 * Noise.noise(2 * nx, 2 * ny) +
+      0.25 * Noise.noise(4 * nx, 4 * ny);
+    const e2 = e / (1 + 0.5 + 0.25);
+    noise = Math.pow(e2, soft);
 
+    if (!isNaN(noise)) {
+      terrainGeometry.attributes.position.array[i + 2] = noise * noiseScale;
+    }
 
-    terrainGeometry.attributes.position.array[i + 2] = noise;
-    mappedValues.push(noise);
     // Calculate the color based on the height
-
-    const color = Math.max(3, 2-noise); // Lower noise -> lighter color (scale 0-1)
-    colors[i] = color; // Set red component
-    colors[i + 1] = color; // Set green component
-    colors[i + 2] = color; // Set blue component
-    terrainGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-
-    
+    const biomeColor = biome(noise);
+    colors[i] = (biomeColor >> 16) & 255;
+    colors[i + 1] = (biomeColor >> 8) & 255;
+    colors[i + 2] = biomeColor & 255;
+    terrainGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
   }
   terrainGeometry.computeVertexNormals();
-  
+
   const terrain = new THREE.Mesh(terrainGeometry, terrainMaterial);
   terrain.rotation.x = -Math.PI / 2; // Rotate for a flat terrain
   terrain.position.y = -50; // Adjust height
@@ -118,37 +145,55 @@ function createTerrain() {
   terrainMesh = terrain;
 }
 
+const WATER = 0x0000ff;
+const BEACH = 0xffff00;
+const FOREST = 0x228b22;
+const JUNGLE = 0x2e8b57;
+const SAVANNAH = 0x9acd32;
+const DESERT = 0xf4a460;
+const SNOW = 0xffffff;
+function biome(e) {
+  // these thresholds will need tuning to match your generator
+  if (e < 0.1) return BEACH;
+  else if (e < 0.2) return FOREST;
+  else if (e < 0.4) return JUNGLE;
+  else if (e < 0.8) return SAVANNAH;
+  else if (e < 0.9) return DESERT;
+  else return SNOW;
+}
+
 // Terrain ekleyin
 createTerrain();
 // Terrain'i belirli aralıklarla yeniden oluşturun (2 saniyede bir)
-const terrainRefreshInterval = 2000;
+const terrainRefreshInterval = 200;
 setInterval(createTerrain, terrainRefreshInterval);
 //createCharts();
 // Scroll Animation
 
-function createCharts(){
-  let chart = new Chart(document.getElementById('myChart'), {
-    type: 'line',
+function createCharts() {
+  let chart = new Chart(document.getElementById("myChart"), {
+    type: "line",
     data: {
-        labels: Array.from({length: mappedValues.length}, (_, i) => i + 1),
-        datasets: [{
-            label: 'Mapped Values',
-            data: mappedValues,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-        }]
+      labels: Array.from({ length: mappedValues.length }, (_, i) => i + 1),
+      datasets: [
+        {
+          label: "Mapped Values",
+          data: mappedValues,
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
 }
-
 
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
